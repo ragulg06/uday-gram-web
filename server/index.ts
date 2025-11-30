48dba8f0a4e3baa2988d3b59e3a70b9fd76d1bb4
“ragulg06” <ragulgunasekarann@gmail.com>
2025-11-30 21:39:25 +0530

    pmagy

--- server/index.ts
+++ server/index.ts
@@ -89,7 +89,7 @@ app.use((req, res, next) => {
     {
       port,
       host: "0.0.0.0",
-      reusePort: true,
+      reusePort: process.platform !== "win32",
     },
     () => {
       log(`serving on port ${port}`);
